63b4a6b5974a1d45d05ee35b66757ecf428a7439
Goni29 <Heat306@gmail.com>
2024-04-08 11:15:01 +0900

    끝 !

--- project/src/main/java/com/lucle/myp/service/ProductServiceImpl.java
+++ project/src/main/java/com/lucle/myp/service/ProductServiceImpl.java
@@ -163,76 +163,76 @@ public class ProductServiceImpl implements ProductService {
 	    List<SearchStatVo> list = mapper.getStatList(cri);
 	    
 	    for(SearchStatVo vo : list) {
-	        Long category = vo.getCATEGORY(); // PCATEGORY 값 가져오기
+	        Long category = vo.getCATEGORY(); // category 값 가져오기
 	        
-	        // PCATEGORY 값에 따라 PCATEGORYName 설정
-	        if (Long.valueOf(1111).equals(pCategory)) {
+	        // category 값에 따라 categoryName 설정
+	        if (Long.valueOf(1111).equals(category)) {
 	            vo.setPname("엘지냉장고");
-	        } else if (Long.valueOf(1112).equals(pCategory)) {
+	        } else if (Long.valueOf(1112).equals(category)) {
 	            vo.setPname("삼성냉장고");
-	        } else if (Long.valueOf(1113).equals(pCategory)) {
+	        } else if (Long.valueOf(1113).equals(category)) {
 	            vo.setPname("위니아냉장고");
-	        } else if (Long.valueOf(1121).equals(pCategory)) {
+	        } else if (Long.valueOf(1121).equals(category)) {
 	            vo.setPname("쿠쿠전기밥솥");
-	        } else if (Long.valueOf(1122).equals(pCategory)) {
+	        } else if (Long.valueOf(1122).equals(category)) {
 	            vo.setPname("쿠첸전기밥솥");
-	        } else if (Long.valueOf(1131).equals(pCategory)) {
+	        } else if (Long.valueOf(1131).equals(category)) {
 	            vo.setPname("엘지인덕션");
-	        } else if (Long.valueOf(1132).equals(pCategory)) {
+	        } else if (Long.valueOf(1132).equals(category)) {
 	            vo.setPname("삼성인덕션");
-	        } else if (Long.valueOf(1133).equals(pCategory)) {
+	        } else if (Long.valueOf(1133).equals(category)) {
 	            vo.setPname("쿠쿠인덕션");
-	        } else if (Long.valueOf(1211).equals(pCategory)) {
+	        } else if (Long.valueOf(1211).equals(category)) {
 	            vo.setPname("엘지TV");
-	        } else if (Long.valueOf(1212).equals(pCategory)) {
+	        } else if (Long.valueOf(1212).equals(category)) {
 	            vo.setPname("삼성TV");
-	        } else if (Long.valueOf(1221).equals(pCategory)) {
+	        } else if (Long.valueOf(1221).equals(category)) {
 	            vo.setPname("엘지프로젝터");
-	        } else if (Long.valueOf(1222).equals(pCategory)) {
+	        } else if (Long.valueOf(1222).equals(category)) {
 	            vo.setPname("삼성프로젝터");
-	        } else if (Long.valueOf(1223).equals(pCategory)) {
+	        } else if (Long.valueOf(1223).equals(category)) {
 	            vo.setPname("엡손프로젝터");
-	        } else if (Long.valueOf(2111).equals(pCategory)) {
+	        } else if (Long.valueOf(2111).equals(category)) {
 	            vo.setPname("엘지데스크탑");
-	        } else if (Long.valueOf(2112).equals(pCategory)) {
+	        } else if (Long.valueOf(2112).equals(category)) {
 	            vo.setPname("삼성데스크탑");
-	        } else if (Long.valueOf(2113).equals(pCategory)) {
+	        } else if (Long.valueOf(2113).equals(category)) {
 	            vo.setPname("한성데스크탑");
-	        } else if (Long.valueOf(2121).equals(pCategory)) {
+	        } else if (Long.valueOf(2121).equals(category)) {
 	            vo.setPname("엘지노트북");
-	        } else if (Long.valueOf(2122).equals(pCategory)) {
+	        } else if (Long.valueOf(2122).equals(category)) {
 	            vo.setPname("삼성노트북");
-	        } else if (Long.valueOf(2123).equals(pCategory)) {
+	        } else if (Long.valueOf(2123).equals(category)) {
 	            vo.setPname("레노버노트북");
-	        } else if (Long.valueOf(2131).equals(pCategory)) {
+	        } else if (Long.valueOf(2131).equals(category)) {
 	            vo.setPname("엘지모니터");
-	        } else if (Long.valueOf(2132).equals(pCategory)) {
+	        } else if (Long.valueOf(2132).equals(category)) {
 	            vo.setPname("삼성모니터");
-	        } else if (Long.valueOf(2133).equals(pCategory)) {
+	        } else if (Long.valueOf(2133).equals(category)) {
 	            vo.setPname("필립스모니터");
-	        } else if (Long.valueOf(2211).equals(pCategory)) {
+	        } else if (Long.valueOf(2211).equals(category)) {
 	            vo.setPname("AMD CPU");
-	        } else if (Long.valueOf(2212).equals(pCategory)) {
+	        } else if (Long.valueOf(2212).equals(category)) {
 	            vo.setPname("인텔 CPU");
-	        } else if (Long.valueOf(2221).equals(pCategory)) {
+	        } else if (Long.valueOf(2221).equals(category)) {
 	            vo.setPname("ASUS 메인보드");
-	        } else if (Long.valueOf(2222).equals(pCategory)) {
+	        } else if (Long.valueOf(2222).equals(category)) {
 	        	vo.setPname("MSI 메인보드");
-	        } else if (Long.valueOf(2223).equals(pCategory)) {
+	        } else if (Long.valueOf(2223).equals(category)) {
 	            vo.setPname("ASROCK 메인보드");
-	        } else if (Long.valueOf(2231).equals(pCategory)) {
+	        } else if (Long.valueOf(2231).equals(category)) {
 	            vo.setPname("삼성 램");
-	        } else if (Long.valueOf(2232).equals(pCategory)) {
+	        } else if (Long.valueOf(2232).equals(category)) {
 	            vo.setPname("SK하이닉스 램");
-	        } else if (Long.valueOf(2233).equals(pCategory)) {
+	        } else if (Long.valueOf(2233).equals(category)) {
 	            vo.setPname("마이크론 램");
-	        } else if (Long.valueOf(2311).equals(pCategory)) {
+	        } else if (Long.valueOf(2311).equals(category)) {
 	            vo.setPname("로지텍 마우스");
-	        } else if (Long.valueOf(2312).equals(pCategory)) {
+	        } else if (Long.valueOf(2312).equals(category)) {
 	            vo.setPname("레이저 마우스");
-	        } else if (Long.valueOf(2321).equals(pCategory)) {
+	        } else if (Long.valueOf(2321).equals(category)) {
 	            vo.setPname("로지텍 키보드");
-	        } else if (Long.valueOf(2322).equals(pCategory)) {
+	        } else if (Long.valueOf(2322).equals(category)) {
 	            vo.setPname("앱코 키보드");
 	        } else {
 	            vo.setPname("미등록상품");
